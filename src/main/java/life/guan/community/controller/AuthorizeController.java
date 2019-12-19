8f42e3cb13ac31fb085a8e9439ade345420eeb86
guanjun <guanjunace@gmail.com>
2019-12-19 17:04:13 +0800

    add session&cookie

--- src/main/java/life/guan/community/controller/AuthorizeController.java
+++ src/main/java/life/guan/community/controller/AuthorizeController.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class AuthorizeController {
     @Autowired
@@ -23,7 +25,8 @@ public class AuthorizeController {
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
-                           @RequestParam(name="state") String state){
+                           @RequestParam(name="state") String state,
+                           HttpServletRequest request){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -32,7 +35,15 @@ public class AuthorizeController {
         accessTokenDTO.setState(state);
         String accesstoken = githubProvider.getAccesstoken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accesstoken);
-        System.out.println(user.getName());
-        return "index";
+        //System.out.println(user.getName());
+        if (user != null) {
+            // 登陆成功 写cookie和session
+            request.getSession().setAttribute("user", user);
+            // 重定向
+            return "redirect:/";
+        } else{
+            return "redirect:/";
+        }
+        //return "index";
     }
 }
